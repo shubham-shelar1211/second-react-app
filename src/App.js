e06baafb00573582239eed032496833d03dddf5b
Shubham Shelar <shubhamshelar1211@gmail.com>
2023-06-14 11:36:00 +0530

    hello

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
-
+// JSX = JS + HTML
+// IF A FUNCTION RETURNS HTML, THIS BECOMES A USER DEFINED TAG
+// <App></App>
+// <App />
+// Compare with PreDefineTag
+// h1, p, li,
+// Why Master Case Convention ?
+// For Pre Define Tag  :: Lowercase :: <h1>
+// For User Define Tag :: MasterCase :: <App>
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <h1>Hello</h1>;
 }
 
+// UNUSED!!!
+// JSX => JS + HTML/XML
+// IF your function returns HTML
+// Convention:: Start your function name in captial letter.
+function Sum() {
+  return <h1>100</h1>;
+}
 export default App;
