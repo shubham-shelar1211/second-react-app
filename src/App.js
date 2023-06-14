e872a84d12561370a9c5767a0315ecd5278dd1aa
Shubham Shelar <shubhamshelar1211@gmail.com>
2023-06-14 13:37:59 +0530

    dumb

--- src/App.js
+++ src/App.js
@@ -2,30 +2,38 @@ function App() {
   return (
     <>
       <h1 className="bg-primary text-light p-3">My Project</h1>
-      <DataBlock />
-      <DataBlock />
-      <DataBlock />
-      <DataBlock />
-      <DataBlock />
-      
-    </>
-  );
-}
 
-function DataBlock() {
-  return (
-    <>
       <div className="alert alert-primary">
         <h1>Lorem ipsum dolor sit amet.</h1>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Esse nihil
-        adipisci ducimus quisquam a rerum accusantium consequuntur. Quasi itaque
-        qui consectetur, culpa iste nam in quibusdam quisquam doloribus, dicta
-        perspiciatis accusamus praesentium! Quos accusantium explicabo alias
-        perspiciatis voluptates totam consectetur laboriosam voluptas cumque,
-        amet quasi vero nostrum quibusdam ad in mollitia soluta! Voluptatem,
-        porro quo ut in et corporis sunt facere accusantium soluta nisi
-        reprehenderit repellat, minus error, veniam ipsa quasi amet. Numquam hic
-        doloribus, aspernatur saepe .
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci,
+        laborum minima doloremque tempora in alias delectus voluptatum ipsum.
+        Deleniti optio ad rem provident esse, veritatis ipsam incidunt. Illum,
+        explicabo omnis. Lorem ipsum dolor sit amet consectetur adipisicing
+        elit. Provident esse tenetur blanditiis voluptatibus ex ab, ad in harum
+        nostrum ipsam quaerat quia dolore suscipit id consequatur quis delectus
+        neque animi?
+      </div>
+
+      <div className="alert alert-primary">
+        <h1>Lorem ipsum dolor sit amet.</h1>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci,
+        laborum minima doloremque tempora in alias delectus voluptatum ipsum.
+        Deleniti optio ad rem provident esse, veritatis ipsam incidunt. Illum,
+        explicabo omnis. Lorem ipsum dolor sit amet consectetur adipisicing
+        elit. Provident esse tenetur blanditiis voluptatibus ex ab, ad in harum
+        nostrum ipsam quaerat quia dolore suscipit id consequatur quis delectus
+        neque animi?
+      </div>
+
+      <div className="alert alert-primary">
+        <h1>Lorem ipsum dolor sit amet.</h1>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci,
+        laborum minima doloremque tempora in alias delectus voluptatum ipsum.
+        Deleniti optio ad rem provident esse, veritatis ipsam incidunt. Illum,
+        explicabo omnis. Lorem ipsum dolor sit amet consectetur adipisicing
+        elit. Provident esse tenetur blanditiis voluptatibus ex ab, ad in harum
+        nostrum ipsam quaerat quia dolore suscipit id consequatur quis delectus
+        neque animi?
       </div>
     </>
   );
