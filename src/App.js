39e3f0944efd807e943032b28dbd6c617cfdbea6
Shubham Shelar <shubhamshelar1211@gmail.com>
2023-06-14 12:45:25 +0530

    external styling

--- src/App.js
+++ src/App.js
@@ -1,28 +1,14 @@
-// HTML + JS => JSX
-// Putting the Brain in HTML.
-// Styling :: Color
-// CSS is also JavaScript!!!
-function App() {
-  // object literals
-  let style = { background: "green", color: "white" };
+import "./App.css";
 
+/**
+ * Bcause 'class' is a keyword in JS.
+ */
+function App() {
   return (
     <>
-      <h1 style={style}>Hello World</h1>
+      <h1 className="beautiful">Hello World</h1>
 
-      {/** First Curly Brace represents JS Expression */}
-      {/** Second Culry Brace, its object literal */}
-      <h1 style={{}}>Hello</h1>
-      <h1 style={{ background: "blue", color: "white" }}>Hello Again</h1>
-
-      {/** Observations */}
-      {/** Style Attributes become CamelCase in Nature */}
-      {/** Values we are wriing in double quotes */}
-      {/** Multiple props are seperate by comma */}
-
-      <h1 style={{ backgroundColor: "red", color: "white", padding: "8px" }}>
-        hello World
-      </h1>
+      <h1 style={{ color: "red" }}>Hello</h1>
     </>
   );
 }
