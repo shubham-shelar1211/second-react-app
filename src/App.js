8e9231726b7d06066c01823d2af70d23ac78ba3a
Shubham Shelar <shubhamshelar1211@gmail.com>
2023-06-14 13:47:35 +0530

    static

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import DataBlock from "./DataBlock";
+
 function App() {
   return (
     <>
@@ -5,27 +7,6 @@ function App() {
       <DataBlock />
       <DataBlock />
       <DataBlock />
-      <DataBlock />
-      <DataBlock />
-    </>
-  );
-}
-
-function DataBlock() {
-  return (
-    <>
-      <div className="alert alert-primary">
-        <h1>Lorem ipsum dolor sit amet.</h1>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Esse nihil
-        adipisci ducimus quisquam a rerum accusantium consequuntur. Quasi itaque
-        qui consectetur, culpa iste nam in quibusdam quisquam doloribus, dicta
-        perspiciatis accusamus praesentium! Quos accusantium explicabo alias
-        perspiciatis voluptates totam consectetur laboriosam voluptas cumque,
-        amet quasi vero nostrum quibusdam ad in mollitia soluta! Voluptatem,
-        porro quo ut in et corporis sunt facere accusantium soluta nisi
-        reprehenderit repellat, minus error, veniam ipsa quasi amet. Numquam hic
-        doloribus, aspernatur saepe .
-      </div>
     </>
   );
 }
