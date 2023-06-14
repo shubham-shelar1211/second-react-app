e1818743fd65e18c6465c99fb0ecdd24c8b51f53
Shubham Shelar <shubhamshelar1211@gmail.com>
2023-06-14 13:27:10 +0530

    dumb

--- src/App.js
+++ src/App.js
@@ -1,10 +1,40 @@
-// import "./App.css";
-import "bootstrap/dist/css/bootstrap.css";
-
 function App() {
   return (
     <>
-      <h1>Hello World</h1>
+      <h1 className="bg-primary text-light p-3">My Project</h1>
+
+      <div className="alert alert-primary">
+        <h1>Lorem ipsum dolor sit amet.</h1>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci,
+        laborum minima doloremque tempora in alias delectus voluptatum ipsum.
+        Deleniti optio ad rem provident esse, veritatis ipsam incidunt. Illum,
+        explicabo omnis. Lorem ipsum dolor sit amet consectetur adipisicing
+        elit. Provident esse tenetur blanditiis voluptatibus ex ab, ad in harum
+        nostrum ipsam quaerat quia dolore suscipit id consequatur quis delectus
+        neque animi?
+      </div>
+
+      <div className="alert alert-primary">
+        <h1>Lorem ipsum dolor sit amet.</h1>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci,
+        laborum minima doloremque tempora in alias delectus voluptatum ipsum.
+        Deleniti optio ad rem provident esse, veritatis ipsam incidunt. Illum,
+        explicabo omnis. Lorem ipsum dolor sit amet consectetur adipisicing
+        elit. Provident esse tenetur blanditiis voluptatibus ex ab, ad in harum
+        nostrum ipsam quaerat quia dolore suscipit id consequatur quis delectus
+        neque animi?
+      </div>
+
+      <div className="alert alert-primary">
+        <h1>Lorem ipsum dolor sit amet.</h1>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci,
+        laborum minima doloremque tempora in alias delectus voluptatum ipsum.
+        Deleniti optio ad rem provident esse, veritatis ipsam incidunt. Illum,
+        explicabo omnis. Lorem ipsum dolor sit amet consectetur adipisicing
+        elit. Provident esse tenetur blanditiis voluptatibus ex ab, ad in harum
+        nostrum ipsam quaerat quia dolore suscipit id consequatur quis delectus
+        neque animi?
+      </div>
     </>
   );
 }
