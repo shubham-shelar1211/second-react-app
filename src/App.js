c7d4c807ade8399fb84475c1b1c8a58364da81a5
Shubham Shelar <shubhamshelar1211@gmail.com>
2023-06-14 12:00:52 +0530

    hello with parent element

--- src/App.js
+++ src/App.js
@@ -1,21 +1,12 @@
-// JSX = JS + HTML
-// IF A FUNCTION RETURNS HTML, THIS BECOMES A USER DEFINED TAG
-// <App></App>
-// <App />
-// Compare with PreDefineTag
-// h1, p, li,
-// Why Master Case Convention ?
-// For Pre Define Tag  :: Lowercase :: <h1>
-// For User Define Tag :: MasterCase :: <App>
+// JSX with 1 Tag or 1 H1
+// JSX with 2 Tag or 2 H1
+// RULE::IF USING MORE THAN 1 TAG; IT MUST BE UUNDER PARENT
 function App() {
-  return <h1>Hello</h1>;
+  return (
+    <>
+      <h1>Hello</h1>
+    </>
+  );
 }
 
-// UNUSED!!!
-// JSX => JS + HTML/XML
-// IF your function returns HTML
-// Convention:: Start your function name in captial letter.
-function Sum() {
-  return <h1>100</h1>;
-}
 export default App;
