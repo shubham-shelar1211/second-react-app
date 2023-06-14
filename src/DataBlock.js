1b55d809935242e7b1ecb553d2eaa7826ad4efc8
Shubham Shelar <shubhamshelar1211@gmail.com>
2023-06-14 13:47:10 +0530

    static website,reusability

---
+++ src/DataBlock.js
@@ -0,0 +1,20 @@
+function DataBlock() {
+    return (
+      <>
+        <div className="alert alert-primary">
+          <h1>Lorem ipsum dolor sit amet.</h1>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Esse nihil
+          adipisci ducimus quisquam a rerum accusantium consequuntur. Quasi itaque
+          qui consectetur, culpa iste nam in quibusdam quisquam doloribus, dicta
+          perspiciatis accusamus praesentium! Quos accusantium explicabo alias
+          perspiciatis voluptates totam consectetur laboriosam voluptas cumque,
+          amet quasi vero nostrum quibusdam ad in mollitia soluta! Voluptatem,
+          porro quo ut in et corporis sunt facere accusantium soluta nisi
+          reprehenderit repellat, minus error, veniam ipsa quasi amet. Numquam hic
+          doloribus, aspernatur saepe .
+        </div>
+      </>
+    );
+  }
+  
+  export default DataBlock;
